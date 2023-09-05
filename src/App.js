56f6cd31d332dae25fd444c3e60e24ce3d24ac53
Andrei des <andoreidesvasiliy@yandex.ru>
2023-09-05 13:46:03 +0300

    -bags

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ function App() {
             return item.id === i ? {...item, completed: !!value} : item
         })
         setTodoList(listMap)
-    }
+    };
 
     const addNewTodo = (text) => {
         if (text) {
             const newTodo = {name: text, completed: false, id: todoList.length + 1};
             setTodoList([...todoList, newTodo]);
         }
-    }
+    };
 
     const todoListMaped = todoList.map((item) => {
         return (
@@ -49,14 +49,14 @@ function App() {
                     <p className='todos__subtitle done'>{item.name}</p>)}
             </div>
         )
-    })
+    });
 
-    const todoListActive = todoList.filter((item) =>{
+    const todoListActive = todoList.filter((item) => {
         return item.completed === false;
-    })
+    });
 
-    const todoListActiveMaped = todoListActive.map((item) =>{
-        return(
+    const todoListActiveMaped = todoListActive.map((item) => {
+        return (
             <div className={'todos__content'} key={item.id}>
                 <input type='checkbox' checked={!!item.completed}
                        onChange={(event) => changeCheckBox(item.id, !!event.target.checked)}
@@ -65,14 +65,14 @@ function App() {
                     <p className='todos__subtitle done'>{item.name}</p>)}
             </div>
         )
-    })
+    });
 
-    const todoListCompleted = todoList.filter((item) =>{
+    const todoListCompleted = todoList.filter((item) => {
         return item.completed === true;
-    })
+    });
 
-    const todoListListCompletedMaped = todoListCompleted.map((item) =>{
-        return(
+    const todoListListCompletedMaped = todoListCompleted.map((item) => {
+        return (
             <div className={'todos__content'} key={item.id}>
                 <input type='checkbox' checked={!!item.completed}
                        onChange={(event) => changeCheckBox(item.id, !!event.target.checked)}
@@ -81,49 +81,58 @@ function App() {
                     <p className='todos__subtitle done'>{item.name}</p>)}
             </div>
         )
-    })
+    });
 
     const handleKeyDown = event => {
         if (event.key === 'Enter') {
             addNewTodo(inputValue);
             setInputValue('');
         }
-    }
+    };
 
-    const chooseActiveBtn = (e) => {
-        setInputActive(true)
-        setInputAll(false)
-        setInputCompleted(false)
-    }
-
-    const chooseAllBtn = (e) => {
-        setInputAll(true)
-        setInputActive(false)
-        setInputCompleted(false)
-    }
-
-    const chooseCompeletedBtn = (e) => {
-        setInputCompleted(true)
-        setInputAll(false)
-        setInputActive(false)
-    }
+    const chooseRadioButton = (str) => {
+        if (str === 'all') {
+            setInputAll(true)
+            setInputActive(false)
+            setInputCompleted(false)
+        }
+        if (str === 'active') {
+            setInputActive(true)
+            setInputAll(false)
+            setInputCompleted(false)
+        }
+        if (str === 'completed') {
+            setInputCompleted(true)
+            setInputAll(false)
+            setInputActive(false)
+        }
+    };
 
     return (
         <div className="App">
             <p className={'todos__title'}>todos</p>
-            <div className="todos__container">
-                <input type={'text'} className={'todos__input'} value={inputValue}
-                       onChange={(event) => setInputValue(event.target.value)} placeholder={'what needs to be done?'}
-                       onKeyDown={handleKeyDown}/>
-            </div>
+            <input type={'text'} className={'todos__input'} value={inputValue}
+                   onChange={(event) => setInputValue(event.target.value)} placeholder={'what needs to be done?'}
+                   onKeyDown={handleKeyDown}/>
             <div className={'todos__map'}>
-                {inputAll ? todoListMaped : inputActive ? todoListActiveMaped : todoListListCompletedMaped }
+                {inputAll ? todoListMaped : inputActive ? todoListActiveMaped : todoListListCompletedMaped}
             </div>
             <div className={'todos__control'}>
                 <p className={'label__paragraph'}>{todoListActiveMaped.length} items left</p>
-                <label className={'label__container'}>all<input type="radio" id={'todos__radio-all'} checked={inputAll} onChange={(event) => chooseAllBtn()} name={'radio'}  /></label>
-                <label className={'label__container'}>active<input type="radio" id={'todos__radio-active'} checked={inputActive} onChange={event => chooseActiveBtn()} name={'radio'} /></label>
-                <label className={'label__container'}>completed<input type="radio" id={'todos__radio-completed'} checked={inputCompleted} onChange={(e)=>chooseCompeletedBtn()}  name={'radio'} /></label>
+                <label className={'label__container'}>all<input className={'input__checked'} type="radio"
+                                                                id={'todos__radio-all'} checked={inputAll}
+                                                                onChange={() => chooseRadioButton('all')}
+                                                                name={'radio'}/></label>
+                <label className={'label__container'}>active<input className={'input__checked'} type="radio"
+                                                                   id={'todos__radio-active'}
+                                                                   checked={inputActive}
+                                                                   onChange={() => chooseRadioButton('active')}
+                                                                   name={'radio'}/></label>
+                <label className={'label__container'}>completed<input className={'input__checked'} type="radio"
+                                                                      id={'todos__radio-completed'}
+                                                                      checked={inputCompleted}
+                                                                      onChange={() => chooseRadioButton('completed')}
+                                                                      name={'radio'}/></label>
             </div>
         </div>
     );
